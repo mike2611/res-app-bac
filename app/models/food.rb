8f66d66991ff709cd49cf62b54dd44daea56cd84
mike2611 <mpuentes2611@gmail.com>
2022-04-05 19:02:45 -0500

    Add validations an relation to models

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,2 +1,6 @@
 class Food < ApplicationRecord
+  has_many :bill_foods
+  validates :name, presence: true
+  validates :price, presence: true
+  validates :price, numericality: { greater_than_or_equal_to: 0 }
 end
