a2852edb7433149ee28ae213ac6ae11d92858725
mike2611 <mpuentes2611@gmail.com>
2022-04-05 22:02:17 -0500

    Add user index to table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_03_31_174415) do
+ActiveRecord::Schema[7.0].define(version: 2022_04_06_003154) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -63,6 +63,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_03_31_174415) do
     t.integer "people"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_tables_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -78,4 +80,5 @@ ActiveRecord::Schema[7.0].define(version: 2022_03_31_174415) do
   add_foreign_key "bill_foods", "foods"
   add_foreign_key "bills", "tables"
   add_foreign_key "bills", "users"
+  add_foreign_key "tables", "users"
 end
