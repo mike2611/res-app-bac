af37ff381acb5f960d1e7006ac5d48518f336347
mike2611 <mpuentes2611@gmail.com>
2022-04-06 20:38:52 -0500

    Add endpoint to drinks_controller

--- app/controllers/drinks_controller.rb
+++ app/controllers/drinks_controller.rb
@@ -1,2 +1,49 @@
 class DrinksController < ApplicationController
+  #Endpoint: /drinks get all the drinks
+  def index
+    @drinks = Drink.all
+    render json: @drinks
+  end
+
+  #Endpoint: /drinks/:id get the drink with id
+  def show
+    @drink = Drink.find(params[:id])
+    render json: @drink
+  end
+
+  #Endpoint: /drinks post a new drink
+  def create
+    @drink = Drink.new(drink_params)
+    if @drink.save
+      render json: @drink, status: :created
+    else
+      render json: @drink.errors, status: :unprocessable_entity
+    end
+  end
+
+  #Endpoint: /drinks/:id put the drink with id
+  def update
+    @drink = Drink.find(params[:id])
+    if @drink.update(drink_params)
+      render json: @drink
+    else
+      render json: @drink.errors, status: :unprocessable_entity
+    end
+  end
+
+  #Endpoint: /drinks/:id delete the drink with id
+  def destroy
+    @drink = Drink.find(params[:id])
+    if @drink.destroy
+      head :no_content
+    else
+      render json: @drink.errors, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def drink_params
+    params.permit(:name, :price)
+  end
 end
