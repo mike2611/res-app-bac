a99e71d15da6ebe65b6ee6cd33650840c4406370
mike2611 <mpuentes2611@gmail.com>
2022-04-06 21:55:51 -0500

    Add endpoints for user_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,49 @@
 class UsersController < ApplicationController
+  #Endpoint: /users get all the users
+  def index
+    @users = User.all
+    render json: @users
+  end
+
+  #Endpoint: /users/:id get the user with id
+  def show
+    @user = User.find(params[:id])
+    render json: @user
+  end
+
+  #Endpoint: /users post a new user
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      render json: @user, status: :created
+    else
+      render json: @user.errors, status: :unprocessable_entity
+    end
+  end
+
+  #Endpoint: /users/:id put the user with id
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      render json: @user
+    else
+      render json: @user.errors, status: :unprocessable_entity
+    end
+  end
+
+  #Endpoint: /users/:id delete the user with id
+  def destroy
+    @user = User.find(params[:id])
+    if @user.destroy
+      head :no_content
+    else
+      render json: @user.errors, status: :unprocessable_entity
+    end
+  end
+
+  private
+  def user_params
+    params.permit(:name, :password)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,13 +3,9 @@ Rails.application.routes.draw do
   resources :bill_foods
   resources :bills
   resources :tables
-  resources :users
-  resources :waiters
-  resources :drinks
+  resources :users, only: [:index, :show ,:create, :update, :destroy]
+  resources :drinks, only: [:index, :show, :create, :update, :destroy]
   resources :foods, only: [:index, :show, :create, :update, :destroy]
-  resources :alimento_cuenta
-  resources :alimentos
-  resources :bebidas
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
